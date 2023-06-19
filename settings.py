633d25c93af128e53bf22ab4c14d58d8f468e529
ericka.bermudez <37844174+ErickaBermudez@users.noreply.github.com>
2023-06-19 15:16:09 +0900

    clean

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 POPULATION_SIZE = 5 # The number of schedules in each generation
 CROSSOVER_RATE = 0.8 # The probability of a crossover occurring
 MUTATION_RATE = 0.2 # The probability of a mutation occurring
-NUMBER_OF_GENERATIONS = 10 # How many generations to run the algorithm for
 ELITISM_COUNT = 1 # How many of the best schedules to keep from one generation to the next
 """
 Selection pressure, a probabilistic measure of a chromosome's likelihood of participation 
